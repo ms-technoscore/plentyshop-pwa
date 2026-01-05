2697551fbe2e51ef564c4d9ac346b7de965a5068
Shikhar Shukla <shikharshukla99@Shikhars-MacBook-Air.local>
2026-01-05 16:11:33 +0530

    hide search bar

--- apps/web/app/components/ui/Header.vue
+++ apps/web/app/components/ui/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <MegaMenu :categories="categoryTree">
     <template v-if="viewport.isGreaterOrEquals('md')">
-      <UiSearch class="hidden md:block flex-1" />
+      <!-- <UiSearch class="hidden md:block flex-1" /> -->
       <nav class="hidden ml-4 md:flex md:flex-row md:flex-nowrap">
         <template v-if="localeCodes.length > 1">
           <UiButton
